6246bf315eddf171a74860a9460a8f11965756be
nileshgulia1 <nileshgulia@gmail.com>
2018-02-16 18:52:50 +0530

    added plone.restapis

--- templates/App.js
+++ templates/App.js
@@ -2,17 +2,48 @@ module.exports = `import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = 'http://localhost:8080/Plone';
+const API_HEADERS = {
+  'Accept': 'application/json',
+  'Authorization': 'Basic Zm9vYmFyOmZvb2Jhcgo='
+};
+
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      page: {}
+    };
+  }
+
+  componentDidMount(){
+    fetch(API_URL + '/front-page', {headers: API_HEADERS})
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({page: responseData});
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Plone</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>{this.state.page.title}</h2>
+        </div>
+        {
+          this.state.page.description &&
+          <h3>{this.state.page.description}</h3>
+        }
+        {
+          this.state.page.text &&
+          <p dangerouslySetInnerHTML={{ __html: this.state.page.text.data }} />
+        }
       </div>
     );
   }
